d31c6c09187f6b2bb2a194a7885117f4fa592fb7
Kelvin30 <kelvin30>
2022-02-25 11:33:00 +0100

    update

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+     <SafeAreaView style={{flex: 1, marginTop: StatusBar.currentHeight}}>
+      <View style={{padding: 16, backgroundColor: "green" }}>
+        <Text>Search</Text>
+      </View>
+      <View style={{flex: 1,padding: 16, backgroundColor: "blue" }}>
+        <Text>List</Text>
+      </View>
+      
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
