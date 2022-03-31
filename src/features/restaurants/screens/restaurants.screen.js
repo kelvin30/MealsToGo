04a3ff45e5f1cd5ed71fadd3f7ea588939eca15b
Kelvin Rumani <39619237+kelvin30@users.noreply.github.com>
2022-03-31 10:31:11 +0200

    Update restaurants.screen.js

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -7,7 +7,7 @@ import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.components";
 import {SafeArea} from "../../../components/utility/safe-area.components"
 import { RestaurantContext } from "../../../services/restaurants/restaurants.context";
-
+import {View} from "react-native";
 
 const SearchContainer = styled.View`
 padding: ${(props) => props.theme.space[3]};
@@ -26,6 +26,18 @@ export const RestaurantsScreen = () => {
  
   return (  
     <SafeArea>
+    {isLoading && (
+        <View style={{position:"absolute", top:"150%", left:"50%"}}>
+              <ActivityIndicator 
+              size={50}
+              style={{marginLeft: -25}}
+              animating={true} 
+              color={Colors.red800} 
+              />
+        </View>
+      )
+
+      }
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
