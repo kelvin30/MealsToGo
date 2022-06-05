9fa19e9c5b3d24514bbddea8b23d8ab902a33eb7
kelvin30 <kelvin.rumani@gmail.com>
2022-06-05 21:36:42 +0200

    Map Navigation

---
+++ src/features/map/screens/map.screen.js
@@ -0,0 +1,6 @@
+import React, { useContext, useState, useEffect} from "react";
+import MapView from "react-native-maps";
+
+
+
+export const MapScreen = () => <MapView style={{ height: "100%" }} />;
