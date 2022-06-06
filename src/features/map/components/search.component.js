35003f233261f9fa5de24d8bfcf5933c0cb20acd
kelvin30 <kelvin.rumani@gmail.com>
2022-06-06 10:53:59 +0200

    map markers

--- src/features/map/components/search.component.js
+++ src/features/map/components/search.component.js
@@ -6,20 +6,23 @@ import { LocationContext } from "../../../services/location/location.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState(keyword);
-
   useEffect(() => {
     setSearchKeyword(keyword);
   }, [keyword]);
-
   return (
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,6 +1,57 @@
 import React, { useContext, useState, useEffect} from "react";
 import MapView from "react-native-maps";
+import styled from "styled-components/native";
 
+import {LocationContext} from "../../../services/location/location.context";
+import { RestaurantContext} from "../../../services/restaurants/restaurants.context"
 
+import { Search } from "../components/search.component";
 
-export const MapScreen = () => <MapView style={{ height: "100%" }} />;
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MapScreen = () => {
+
+const { location } = useContext(LocationContext);
+const { restaurants = [] } = useContext(RestaurantContext);
+
+const [latDelta, setLatDelta] = useState(0);
+
+const {lat, lng, viewport } = location;
+
+useEffect(() => {
+const northeastLat = viewport.northeast.lat;
+const southwestLat = viewport.southwest.lat;
+
+setLatDelta( northeastLat - southwestLat );
+},[location, viewport]);
+
+return (
+  <>
+    <Search />
+    <Map
+      region={{
+        latitude: lat,
+        longitude: lng,
+        latitudeDelta: latDelta,
+        longitudeDelta: 0.02,
+      }}
+    >
+      {restaurants.map((restaurant) => {
+        return <MapView.Marker
+        key={restaurant.name}
+        title={restaurant.name}
+        coordinate={{
+          latitude: restaurant.geometry.location.lat,
+          longitude: restaurant.geometry.location.lng,
+        }}
+        >
+
+        </MapView.Marker>;
+      })}
+    </Map>
+  </>
+);
+}
