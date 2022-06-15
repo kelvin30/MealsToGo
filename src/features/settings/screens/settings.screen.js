06a247b70d4423b8498e0328cd3767ca772cd6da
kelvin30 <kelvin.rumani@gmail.com>
2022-06-15 11:13:24 +0200

    settings page fix

---
+++ src/features/settings/screens/settings.screen.js
@@ -0,0 +1,48 @@
+import React, { useContext } from "react";
+import styled from "styled-components/native";
+import { List, Avatar} from "react-native-paper";
+
+import { Text } from "../../../components/typography/text.components";
+import { Spacer } from "../../../components/spacer/spacer.components";
+
+import { SafeArea } from "../../../components/utility/safe-area.components";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+
+const SettingsItem = styled(List.Item)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
+
+
+
+export const SettingsScreen = ({ navigation }) => {
+  const { onLogout, user } = useContext(AuthenticationContext);
+  return (
+    <SafeArea>
+     <AvatarContainer>
+        <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <Spacer position="top" size="large">
+          <Text variant="label">{user.email}</Text>
+        </Spacer>
+      </AvatarContainer>
+      <List.Section>
+        
+      <SettingsItem
+          title="Favourites"
+          description="View your favourites"
+          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
+          onPress={() => navigation.navigate("Favourites")}
+        />
+        <SettingsItem
+          style={{ padding: 16 }}
+          title="Logout"
+          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+          onPress={onLogout}
+        />
+      </List.Section>
+    </SafeArea>
+  );
+};
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-import { Text, Button } from "react-native";
 import { MapScreen } from "../../features/map/screens/map.screen";
-import { SafeArea } from "../../components/utility/safe-area.components";
+import { SettingsNavigator } from "./settings.navigator";
 
-import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 import { RestaurantsNavigator } from "./restaurants.navigator";
 
@@ -21,16 +19,6 @@ const TAB_ICON = {
   Restaurants: "md-restaurant",
   Map: "md-map",
   Settings: "md-settings",
-};
-
-const Settings = () => {
-  const { onLogout } = useContext(AuthenticationContext);
-  return (
-    <SafeArea>
-      <Text>Settings</Text>
-      <Button title="logout" onPress={() => onLogout()} />
-    </SafeArea>
-  );
 };
 
 
@@ -57,7 +45,7 @@ export const AppNavigator = () => (
         >
           <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
           <Tab.Screen name="Map" component={MapScreen} />
-          <Tab.Screen name="Settings" component={Settings} />
+          <Tab.Screen name="Settings" component={SettingsNavigator} />
         </Tab.Navigator>
       </RestaurantContextProvider>
     </LocationContextProvider>
