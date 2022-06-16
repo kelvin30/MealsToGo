7783e4a4df0f7982c28f9c88fd2700dcd11dc752
kelvin30 <kelvin.rumani@gmail.com>
2022-06-16 12:59:53 +0200

    Camera set

---
+++ src/features/settings/screens/camera.screen.js
@@ -0,0 +1,59 @@
+import React, {useRef, useState, useEffect, useContext} from "react";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
+import { View, TouchableOpacity } from "react-native";
+import { Text } from "../../../components/typography/text.components";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+
+
+
+const ProfileCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
+
+const InnerSnap = styled.View`
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+`;
+
+export const CameraScreen = ({ navigation }) => {
+    const [hasPermission, setHasPermission] = useState(null);
+    const cameraRef = useRef();
+    const { user } = useContext(AuthenticationContext);
+    const snap = async () => {
+      if (cameraRef) {
+        const photo = await cameraRef.current.takePictureAsync();
+        console.log(photo)
+        AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+        navigation.goBack();
+      }
+    };
+    useEffect(() => {
+      (async () => {
+        const { status } = await Camera.requestPermissionsAsync();
+        setHasPermission(status === "granted");
+      })();
+    }, []);
+    if (hasPermission === null) {
+      return <View />;
+    }
+    if (hasPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+        <ProfileCamera
+      ref={(camera) => (cameraRef.current = camera)}
+      type={Camera.Constants.Type.front}
+    >
+      <TouchableOpacity onPress={snap}>
+        <InnerSnap />
+      </TouchableOpacity>
+    </ProfileCamera>
+       )
+};
--- src/infrastructure/navigation/settings.navigator.js
+++ src/infrastructure/navigation/settings.navigator.js
@@ -3,6 +3,8 @@ import { SettingsScreen } from "../../features/settings/screens/settings.screen"
 
 import { FavouritesScreen } from "../../features/settings/screens/favourites.screen";
 
+import { CameraScreen } from "../../features/settings/screens/camera.screen";
+
 import {
   createStackNavigator,
   CardStyleInterpolators,
@@ -26,6 +28,7 @@ export const SettingsNavigator = ({ route, navigation }) => {
         component={SettingsScreen}
       />
       <SettingsStack.Screen name="Favourites" component={FavouritesScreen} />
+      <SettingsStack.Screen name="Camera" component={CameraScreen} />
     </SettingsStack.Navigator>
   );
 };
